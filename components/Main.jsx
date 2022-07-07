afee964704f390c37b11616be946d5c2673e5de7
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-07-07 12:26:32 +0430

    add components folder

---
+++ components/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="w-full flex">
+      <div className="w-[20%] bg-slate-400">right side</div>
+      <div className="w-[80%] bg-yellow-200">left side</div>
+    </div>
+  );
+};
+
+export default Main;
