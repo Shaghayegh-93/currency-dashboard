15a253620be250aeec2fdd83c46b7fdf906bd47e
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-07-09 16:59:58 +0430

    fetch data and import Login and Dropdown component

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import Login from "./Login";
+import Dropdown from "./Dropdown";
 
 const Main = () => {
+  useEffect(() => {
+    const sendRequest = () => {
+      axios({
+        method: "get",
+        url: "https://rest.coinapi.io/v1/exchangerate/BTC?invert=false",
+
+        headers: {
+          "X-CoinAPI-Key": "B85F6259-C353-43D0-B86C-BA852AD5211A",
+        },
+      })
+        .then((res) => {
+          console.log("res: ", res);
+        })
+        .catch((err) => {
+          console.log("err: ", err);
+        });
+    };
+    sendRequest();
+  }, []);
   return (
     <div className="w-full flex">
-      <div className="w-[20%] bg-slate-400">right side</div>
-      <div className="w-[80%] bg-yellow-200">left side</div>
+      <div className="w-[20%] bg-slate-400 h-screen ">left side</div>
+      <div className="w-[80%] flex flex-col">
+        <Login />
+        <Dropdown />
+      </div>
     </div>
   );
 };
